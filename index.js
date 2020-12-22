6208126869f14307c059488e8342f5ca4ff17762
Angel Rego <angelrego@gmail.com>
2020-12-22 14:24:03 +0100

    serial: use this app as a secure radio transmitter
    This app retrieves SECURE data, so its only mission (for now) is to transmit those data. Currently there is only one legitimate receiver, the health app, so a simple HTTP REST call will be transmitted instead of broadcasting or sending the data to a comunications broker.
    
    Note also that the data is not stored here.

---
+++ index.js
@@ -0,0 +1,24 @@
+const util = require('./util')
+const fetch = require('node-fetch')
+
+const USB_ADDRESS = '/dev/ttyACM0' // CHANGE THIS
+const BAUDRATE = 115200
+const ENDPOINT = "http://localhost:9090/sensors"
+
+util.createPortSync(USB_ADDRESS, BAUDRATE, processor)
+
+async function processor(data) {
+  console.log(data)
+  if (data.includes(' ')) return
+  try {
+    console.log('sending msg to server: ' + data)
+    await send(data)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function send(msg) {
+  const res = await fetch(ENDPOINT, { method: 'post', body: msg })
+  if (!res.ok) throw res
+}
